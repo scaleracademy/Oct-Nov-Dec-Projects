c274a6e869703a4692d130dc9c3a24ba8335dd29
Arnav Gupta <arnav@codingblocks.com>
2021-07-27 23:58:52 +0530

    multi-threading example
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- 01-JavaBasics/src/main/java/com/scaler/javabasics/Main.java
+++ 01-JavaBasics/src/main/java/com/scaler/javabasics/Main.java
@@ -7,9 +7,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        var downloader = new ExampleComDownloader();
-        System.out.println(
-                downloader.getWebpageSource()
-        );
+//        var downloader = new ExampleComDownloader();
+//        System.out.println(
+//                downloader.getWebpageSource()
+//        );
+
+        var mt = new MultiThreading();
+        mt.runAllThreads();
     }
 }
---
+++ 01-JavaBasics/src/main/java/com/scaler/javabasics/MultiThreading.java
@@ -0,0 +1,34 @@
+package com.scaler.javabasics;
+
+import java.util.PriorityQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class MultiThreading {
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("iteration " + i + " running on " + Thread.currentThread().getName());
+            }
+        }
+    };
+
+    ThreadPoolExecutor executor = new ThreadPoolExecutor(
+            1,
+            5,
+            1L,
+            TimeUnit.SECONDS,
+            new PriorityBlockingQueue<Runnable>()
+    );
+
+    void runAllThreads() {
+        // Q: make three runs of r parallelly on the executor
+        executor.execute(r);
+        executor.execute(r);
+        executor.execute(r);
+    }
+
+
+}
