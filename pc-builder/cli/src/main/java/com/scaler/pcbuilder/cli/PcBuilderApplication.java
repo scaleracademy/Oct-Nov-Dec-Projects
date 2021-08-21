0b30d2290bb5dd35735e5bbfe962520257f289cb
Arnav Gupta <arnav@codingblocks.com>
2021-08-21 22:53:26 +0530

    split our project into 2 modules
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- pc-builder/cli/src/main/java/com/scaler/pcbuilder/cli/PcBuilderApplication.java
+++ pc-builder/cli/src/main/java/com/scaler/pcbuilder/cli/PcBuilderApplication.java
@@ -1,10 +1,7 @@
-package com.scaler.pcbuilder;
+package com.scaler.pcbuilder.cli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.lang.annotation.Inherited;
 
 @SpringBootApplication()
 public class PcBuilderApplication {
